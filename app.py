e5a090489fb5bc3e6887940819f0f0410f1c9ff1
Sxvaaze <72252565+Sxvaaze@users.noreply.github.com>
2022-03-23 14:21:03 +0200

    Version V1.0.0
    First & basic version of the PseudoAPI

---
+++ app.py
@@ -0,0 +1,47 @@
+import main as skroutzapi
+"""
+          _____                    _____                    _____                   _______         
+         /\    \                  /\    \                  /\    \                 /::\    \        
+        /::\    \                /::\    \                /::\____\               /::::\    \       
+       /::::\    \              /::::\    \              /::::|   |              /::::::\    \      
+      /::::::\    \            /::::::\    \            /:::::|   |             /::::::::\    \     
+     /:::/\:::\    \          /:::/\:::\    \          /::::::|   |            /:::/~~\:::\    \    
+    /:::/  \:::\    \        /:::/__\:::\    \        /:::/|::|   |           /:::/    \:::\    \   
+   /:::/    \:::\    \      /::::\   \:::\    \      /:::/ |::|   |          /:::/    / \:::\    \  
+  /:::/    / \:::\    \    /::::::\   \:::\    \    /:::/  |::|___|______   /:::/____/   \:::\____\ 
+ /:::/    /   \:::\ ___\  /:::/\:::\   \:::\    \  /:::/   |::::::::\    \ |:::|    |     |:::|    |
+/:::/____/     \:::|    |/:::/__\:::\   \:::\____\/:::/    |:::::::::\____\|:::|____|     |:::|    |
+\:::\    \     /:::|____|\:::\   \:::\   \::/    /\::/    / ~~~~~/:::/    / \:::\    \   /:::/    / 
+ \:::\    \   /:::/    /  \:::\   \:::\   \/____/  \/____/      /:::/    /   \:::\    \ /:::/    /  
+  \:::\    \ /:::/    /    \:::\   \:::\    \                  /:::/    /     \:::\    /:::/    /   
+   \:::\    /:::/    /      \:::\   \:::\____\                /:::/    /       \:::\__/:::/    /    
+    \:::\  /:::/    /        \:::\   \::/    /               /:::/    /         \::::::::/    /     
+     \:::\/:::/    /          \:::\   \/____/               /:::/    /           \::::::/    /      
+      \::::::/    /            \:::\    \                  /:::/    /             \::::/    /       
+       \::::/    /              \:::\____\                /:::/    /               \::/____/        
+        \::/____/                \::/    /                \::/    /                 ~~              
+         ~~                       \/____/                  \/____/            
+
+                            SkroutzPseudo API V1.0.0 by github.com/Sxvaaze
+"""
+
+#If link is empty or not a valid skroutz product url (does not contain "skroutz.gr/s/") then all api calls will return an error defined by function filter_input in main.py
+link = input("Give url: ")
+
+#Demo 1 (API request for an item's data with no show_all & no limit)
+#Returns base data and info of first store
+res1 = skroutzapi.call(link)
+
+#Demo 2 (API request for an item with show_all & no limit (Returns data from ALL STORES))
+#Returns base data and info for all stores 
+res2 = skroutzapi.call(link, show_all=True)
+
+#Demo 3 (API request for an item with show_all & a limit (Returns ALL STORES if store_count <= limit, else returns data from up to << LIMIT >> stores))
+#Returns base data and info for up to << limit >> stores
+res3 = skroutzapi.call(link, show_all=True, limit=6)
+
+print(res1)
+print('-' * 100)
+print(res2)
+print('-' * 100)
+print(res3)
---
+++ main.py
@@ -0,0 +1,75 @@
+import requests
+from bs4 import BeautifulSoup
+
+def filter_input(link):
+    should_ret = False
+    if link == "err" or "skroutz.gr/s/" not in link:
+        print('-' * 45)
+        print('Error loading requested URL: No URL was given\nor given URL is not a valid skroutz.gr/s/ URL')
+        print('-' * 45)
+        should_ret = True
+    return should_ret
+
+def call(link="err", **kwargs):
+    if filter_input(link):
+        return
+    
+    kwarg_dict = dict()
+    for key,value in kwargs.items():
+        kwarg_dict[key] = value
+    
+    page = requests.get(link, headers={"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'})
+    soup = BeautifulSoup(page.content, 'html.parser')
+    show = False
+    if len(kwarg_dict.keys()) > 0 and 'show_all' in kwarg_dict.keys():
+        show = kwarg_dict['show_all']
+    
+    title = soup.select_one('.page-title').get_text()
+    if not show:
+        price = soup.find("strong", {"class": "dominant-price"}).get_text()
+    else:
+        lim = 64 if 'limit' not in kwarg_dict.keys() else kwargs['limit']
+        divs = soup.findAll("li", {"class": "js-product-card"}, limit=lim)
+        titles = []
+        prices_no_fees = []
+        for i in range(len(divs)):
+            titles.append(divs[i].findChildren("a", {"class": "js-product-link"})[0].get_text())
+            to_append = divs[i].findChildren("strong", {"class": "dominant-price"})[0].get_text()
+            to_append = to_append[:len(to_append)-2].split(",")
+            euros = float(to_append[0])
+            cents = int(to_append[1])
+            final = euros + cents / 10
+            prices_no_fees.append(final)
+
+    rating_count = soup.find("div", {"class": "actual-rating"}).get_text()
+    rating_score_scrape = soup.find("a", {"class": ["rating", "big_stars"]})['title'][0:3].split(",") # Future to-do: make this work with child properties for fuck's shake.
+    rating_score = int(rating_score_scrape[0]) + int(rating_score_scrape[1]) / 10
+
+    discussion_count = soup.find("a", {"href": "#qna"}).find("span").get_text()
+    discussion_count = discussion_count.replace('(', '')
+    discussion_count = discussion_count.replace(')', '')
+
+    sale_badge = soup.find("span", {"class": ["badge", "pricedrop"]})
+
+    is_on_sale = sale_badge is not None
+
+    store_count = soup.find("a", {"href": "#shops"}).findChildren("span")[0].get_text()
+    store_count = store_count.replace('(', '')
+    store_count = store_count.replace(')', '')
+
+    lowest_base_price = price if not show else prices_no_fees[1]
+    max_base_price = price if not show else prices_no_fees[-1]
+        
+    return_dict = {
+        "product_name": title,
+        "base_price": price if not show else prices_no_fees, #returns a list of all base prices of product from all stores if show_all is True, else returns first price found (cheapest)
+        "store_names": titles if show else None, #returns only if show_all is True
+        "store_count": store_count,
+        "rating_count": rating_count,
+        "rating_score": rating_score,
+        "discussion_count": discussion_count,
+        "lowest_price": lowest_base_price,
+        "max_price": max_base_price,
+        "is_on_sale": is_on_sale
+    }
+    return return_dict
